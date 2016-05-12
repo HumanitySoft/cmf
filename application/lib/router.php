6d4e25239947095be5fc052cb3126f3675f6e7d4
Andrey Xezzus <xezzus@gmail.com>
2016-05-12 14:10:12 +0300

    Developing

--- application/lib/router.php
+++ application/lib/router.php
@@ -4,7 +4,10 @@ return function(){
 
   switch(substr($accept,0,6)){
     case "api://":
-      $this->exec(substr($accept,6),file_get_contents('php://input'));
+      $res = $this->exec(substr($accept,6),file_get_contents('php://input'));
+      header('Content-Type: application/json');
+      $res = json_encode($res);
+      die($res);
     break;
     default:
       $this->view('pages');
